c54b4fb1eedfbf8cbb64d8e40975a2e61f8adc4c
Surya <contact@thoughtgeek.com>
2020-06-14 06:24:14 +0530

    Added procfile

--- thoughtgeek/settings.py
+++ thoughtgeek/settings.py
@@ -92,7 +92,7 @@ USE_MODELTRANSLATION = False
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -343,3 +343,5 @@ else:
 
 BLOG_SLUG = ""
 SEARCH_MODEL_CHOICES = ('blog.BlogPost')
+
+SECRET_KEY = '_t_j^v@(raai&m9wslmlc+1=zvvdjzdo_!77sx%w%y(&8rf0%_'
